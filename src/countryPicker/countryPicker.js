4fe54c93d349235db994df71f7d2e9375664d459
Mathieu Enault <me94110@gmail.com>
2020-07-30 19:07:43 +0200

    modificating countryPicker margin space and debugging global select after country selected

--- src/countryPicker/countryPicker.js
+++ src/countryPicker/countryPicker.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { NativeSelect, FormControl } from "@material-ui/core";
+import { NativeSelect, FormControl, StylesProvider } from "@material-ui/core";
 import { fetchCountries } from "../api";
+import styles from './countryPicker.module.css';
 
 const CountryPicker = ({ handleCountryChange }) => {
   const [countriesData, setCountriesData] = useState([]);
@@ -12,19 +13,22 @@ const CountryPicker = ({ handleCountryChange }) => {
     fetchApi();
   }, [setCountriesData]);
  console.log(countriesData);
-  return (
-    <FormControl>
-      <NativeSelect
-        defaultValue=""
-        onChange={(e) => handleCountryChange(e.target.value)}
-      >
-        <option value="global">Global</option>
-        {countriesData.map((country) => (
-          <option key={country}>{country}</option>
-        ))}
-      </NativeSelect>
-    </FormControl>
-  );
+    return (
+
+      <div className={styles.picker}>
+        <FormControl>
+          <NativeSelect
+            defaultValue=""
+            onChange={(e) => handleCountryChange(e.target.value)}
+          >
+            <option value="">Global</option>
+            {countriesData.map((country) => (
+              <option key={country}>{country}</option>
+            ))}
+          </NativeSelect>
+        </FormControl>
+      </div>
+    );
 };
 
 export default CountryPicker;
